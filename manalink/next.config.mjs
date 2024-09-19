253401c077bfa8e1a57b068f3f8a041ad43c98d4
JosephGros <wiley.joseph.gros@gmail.com>
2024-09-19 12:23:04 +0200

    Snälla

--- manalink/next.config.mjs
+++ manalink/next.config.mjs
@@ -1,10 +1,4 @@
-import withPWAInit from "@ducanh2912/next-pwa";
 /** @type {import('next').NextConfig} */
-
-const withPWA = withPWAInit({
-  dest: "public",
-});
-
 const nextConfig = {};
 
-export default withPWA(nextConfig);
+export default nextConfig;
